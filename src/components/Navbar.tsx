bfe4b140897a08ae8a6dcc7626868228bc1c7103
AnhLND <AnhLND3999@gmail.com>
2025-06-09 11:32:19 +0700

    commit code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -58,9 +58,7 @@ const Navbar = () => {
               <a href="#about">About</a>
             </li>
             <li className="ml-2 text-sm uppercase btn-hover">
-              <Link href="/#skills" prefetch={false}>
-                Skills
-              </Link>
+              <a href="#skills">Skills</a>
             </li>
             <li className="ml-2 text-sm uppercase btn-hover">
               <Link href="/#experience">Experience</Link>
