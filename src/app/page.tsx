2e4ea79eb91121526f391e952f38324219dffd59
AnhLND <AnhLND3999@gmail.com>
2025-06-09 10:59:19 +0700

    fix CICD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
 import Main from "@/components/Main";
-import Projects from "@/components/Projects";
+// import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 
 export default function Home() {
